5351b1746eff23935d58ff07bdd458f8a71f609f
bjornthor21 <89970821+bjornthor21@users.noreply.github.com>
2023-11-24 00:07:11 +0000

    ég notaði ekki allar scripturnar sem okkur var gefið

---
+++ Klikk.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Klikk : MonoBehaviour
+{
+    // þegar ýtt er á takka þá byrjar leikurinn
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Shooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    // Bullet og hraði
+    public GameObject bullet;
+    public float speed = 4000f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Þegar ýtt er á Z þá skýst kúla
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("skotið");       
+            
+            // býr til nýtt gameobject kúlu
+            GameObject instBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            // bætir rigidbody við
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            // látið kúlluna fljúga áfram með hraða
+            instBulletRigidbody.AddForce(transform.forward * speed);
+            // eytt kúluni eftir 2 sek
+            Destroy(instBullet, 2f);//þessu var breytt frá 0.5 sek uppi 2 til að hitta í kassan á eyjunni
+           
+        }
+    }
+}
